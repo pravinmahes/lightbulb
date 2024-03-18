3c3c6dcc6056487d62a97accbe76151ccb3d7502
pravinmahes <157895055+pravinmahes@users.noreply.github.com>
2024-03-18 16:09:35 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,9 @@
+function turnOff() {
+    var lightBulb = document.getElementById('img');
+    lightBulb.src = './toppng.com-light-bulb-on-off-png-512x512.png';
+}
+
+function turnOn() {
+    var lightBulb = document.getElementById('img');
+    lightBulb.src='./6082678-removebg-preview (1).png';
+}
